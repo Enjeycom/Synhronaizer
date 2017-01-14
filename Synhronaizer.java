fe7d31199ca5d4eec567991e09180b3c7157b75a
Enjeycom <enjeycom@gmail.com>
2017-01-14 22:31:34 +0300

    Update Synhronaizer.java

--- Synhronaizer.java
+++ Synhronaizer.java
@@ -35,7 +35,7 @@ public class Synhronaizer extends Thread{
 	}
 	//функция удаляет файлы отсутствующия в источнике
 	public  void scanFolderDelete(File folder) throws IOException{
-		gui.infoPanel.plusInfo("Óäàëåíèå â ïàïêå:",folder.getAbsolutePath());
+		gui.infoPanel.plusInfo("Удаление в папке:",folder.getAbsolutePath());
 		File files[]=folder.listFiles();
 		for(int i=0;i<files.length;i++){
 			File tmp=new File(oldAdress(files[i].getAbsolutePath()));
@@ -43,7 +43,7 @@ public class Synhronaizer extends Thread{
 				scanFolderDelete(files[i]);
 			}else
 				if(!tmp.exists()&&!tmp.getName().equals("Indexed Locations.search-ms")){
-					gui.infoPanel.plusInfo("Óäàëåíèå ôàéëà",tmp.getAbsolutePath());
+					gui.infoPanel.plusInfo("Удаление файла",tmp.getAbsolutePath());
 					Files.delete(files[i].toPath());
 					n_delete_files++;
 				}
@@ -58,7 +58,7 @@ public class Synhronaizer extends Thread{
 	}
 	//функция реверсивно синхронизирует папки
 	public  void scanFolder(File folder) throws IOException, NoSuchAlgorithmException{
-		gui.infoPanel.plusInfo("Cèíõðîíèçàöèÿ ïàïêè:",folder.getAbsolutePath());
+		gui.infoPanel.plusInfo("Cинхронизация папки:",folder.getAbsolutePath());
 		File files[]=folder.listFiles();
 		for(int i=0;i<files.length;i++){
 			File tmp=new File(newAdress(files[i].getAbsolutePath()));
@@ -73,7 +73,7 @@ public class Synhronaizer extends Thread{
 					copy(files[i],tmp); //Копирование в случае если файла нет либо стоит галка заменять файлы
 				}else
 				if((settings.old_update&&tmp.lastModified()<files[i].lastModified())){
-					copy(files[i],tmp); //Копирование в случае если файл существует и стоит галка заменять устаревгие файлы
+					copy(files[i],tmp);	//Копирование в случае если файл существует и стоит галка заменять устаревгие файлы
 				}else
 				if(settings.old_update&&tmp.lastModified()==files[i].lastModified()){
 					if(!controlSumChek(tmp,files[i]))
@@ -85,17 +85,17 @@ public class Synhronaizer extends Thread{
 	}
 	//функция считает контрольную сумму
 	public static String getControlSum(File file) throws NoSuchAlgorithmException, IOException {
-         final MessageDigest md = MessageDigest.getInstance("SHA-1");
-         final FileInputStream fis = new FileInputStream(file);
-         byte[] dataBytes = new byte[1024];
-         int bytesRead;
-         while((bytesRead = fis.read(dataBytes)) > 0)
-             md.update(dataBytes, 0, bytesRead);
-         byte[] mdBytes = md.digest();
-         StringBuilder sb = new StringBuilder();
-         for(int i = 0; i < mdBytes.length; i++)
-             sb.append(Integer.toString((mdBytes[i] & 0xff) + 0x100, 16).substring(1));
-         return sb.toString();
+        	final MessageDigest md = MessageDigest.getInstance("SHA-1");
+        	final FileInputStream fis = new FileInputStream(file);
+        	byte[] dataBytes = new byte[1024];
+        	int bytesRead;
+         	while((bytesRead = fis.read(dataBytes)) > 0)
+             		md.update(dataBytes, 0, bytesRead);
+         	byte[] mdBytes = md.digest();
+         	StringBuilder sb = new StringBuilder();
+         	for(int i = 0; i < mdBytes.length; i++)
+             		sb.append(Integer.toString((mdBytes[i] & 0xff) + 0x100, 16).substring(1));
+         	return sb.toString();
 	}
 	//фунция сравнивает контрольные суммы
 	private boolean controlSumChek(File f1, File f2) throws NoSuchAlgorithmException, IOException {
@@ -106,35 +106,35 @@ public class Synhronaizer extends Thread{
 	//поток синхронизации
 	@Override
 	public void run(){
-			if(!new File(settings.inFolder).exists()||!new File(settings.outFolder).exists()){
-				JOptionPane.showMessageDialog(gui,"Вы не выбрали папки или папки не существуют!","Ошибка!",JOptionPane.WARNING_MESSAGE);
-				return;
-			}
-			if(settings.inFolder.equals(settings.outFolder)){
-				JOptionPane.showMessageDialog(gui,"Папке не нужно синхронизироватся самой с собой!","Ошибка!",JOptionPane.WARNING_MESSAGE);
-				return;
-			}
-			double time=System.currentTimeMillis();
-			gui.setContentPane(gui.infoPanel);
-			gui.infoPanel.ok.setEnabled(false);
-			gui.infoPanel.plusInfo("Синхронизация...","");
-			gui.setVisible(true);
+		if(!new File(settings.inFolder).exists()||!new File(settings.outFolder).exists()){
+			JOptionPane.showMessageDialog(gui,"Вы не выбрали папки или папки не существуют!","Ошибка!",JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		if(settings.inFolder.equals(settings.outFolder)){
+			JOptionPane.showMessageDialog(gui,"Папке не нужно синхронизироватся самой с собой!","Ошибка!",JOptionPane.WARNING_MESSAGE);
+			return;
+		}
+		double time=System.currentTimeMillis();
+		gui.setContentPane(gui.infoPanel);
+		gui.infoPanel.ok.setEnabled(false);
+		gui.infoPanel.plusInfo("Синхронизация...","");
+		gui.setVisible(true);
+		try {
+			scanFolder(new File(settings.inFolder));
+			for(Thread t:tasks)
+				t.join();
+		} catch (InterruptedException | NoSuchAlgorithmException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		gui.infoPanel.plusInfo("Удаление...","");
+		if(settings.delete)
 			try {
-				scanFolder(new File(settings.inFolder));
-				for(Thread t:tasks)
-					t.join();
-			} catch (InterruptedException | NoSuchAlgorithmException | IOException e) {
+				scanFolderDelete(new File(settings.outFolder));
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			gui.infoPanel.plusInfo("Удаление...","");
-			if(settings.delete)
-				try {
-					scanFolderDelete(new File(settings.outFolder));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			gui.infoPanel.ok.setEnabled(true);
-			gui.infoPanel.setInfo("Синхронизация завершена!\nСинхронизированно " + n_synh_files+" файлов\nУдалено " + n_delete_files+" ôàéëîâ\nÏðîøëî âåðìåíè: "+(System.currentTimeMillis()-time)/1000+" ñåêóíä");
+		gui.infoPanel.ok.setEnabled(true);
+		gui.infoPanel.setInfo("Синхронизация завершена!\nСинхронизированно " + n_synh_files+" файлов\nУдалено " + n_delete_files+" файлов\nПрошло вермени: "+(System.currentTimeMillis()-time)/1000+" секунд");
 	}
 }
